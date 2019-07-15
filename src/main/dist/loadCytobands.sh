dc7cee218bcee5eb2ac36206d13ccf5e9d45fb4d
Marek Tutaj <mtutaj@mcw.edu>
2019-07-15 12:06:01 -0500

    cytoband loader: added source url for bonobo

--- src/main/dist/loadCytobands.sh
+++ src/main/dist/loadCytobands.sh
@@ -6,7 +6,6 @@
 . /etc/profile
 APPDIR=/home/rgddata/pipelines/ChromosomePipeline
 cd $APPDIR
-# set variable HOST to uppercase hostname (f.e. KIRWAN, KYLE)
 HOST=`hostname -s | tr '[a-z]' '[A-Z]'`
 
 $APPDIR/_run.sh --load cytoband --map_key $1
